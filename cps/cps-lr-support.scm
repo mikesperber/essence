f8e8f0daa8add7b4480aacc786cd71ca8bbb8c20
sperber <devnull@localhost>
1997-02-02 16:03:38 +0100

    Obsolete.

--- cps/cps-lr-support.scm
+++
@@ -1,6 +0,0 @@
-(load "~/soft/pe/similix-support/defprim-r4rs.scm")
-(load "~/soft/pe/similix-support/defconstr.scm")
-(load "~/soft/pe/lr-essence/common/util.adt")
-(load "~/soft/pe/lr-essence/common/grammar.adt")
-(load "~/soft/pe/lr-essence/common/lookahead.scm")
-
