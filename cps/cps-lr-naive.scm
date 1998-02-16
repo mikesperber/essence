ffc1a83d3b1ab493128056dbe4fb24ae00f12839
sperber <devnull@localhost>
1998-02-16 10:59:51 +0100

    Now does lookahead checking.

--- cps/cps-lr-naive.scm
+++ cps/cps-lr-naive.scm
@@ -1,14 +1,16 @@
 ;; Essential LR parsing (continuation-based)
 ;; =========================================
 
-(define (cps-parse grammar state continuations input)
-  (let ((closure (compute-lr-closure state grammar 0 '())))
+(define (cps-parse grammar k first-map
+		   state continuations input)
+  (let ((closure
+	 (compute-lr-closure state grammar k first-map)))
 
     (define (c0 symbol input)
       (cond
        ((not (and (initial? state grammar)
 		  (equal? (grammar-start grammar) symbol)))
-	(cps-parse grammar
+	(cps-parse grammar k first-map
 		   (goto closure symbol)
 		   (cons c0
 			 (take (- (active (goto closure symbol)) 1)
@@ -21,16 +23,16 @@
 	   (member (car (stream-car input))
 		   (next-terminals closure grammar)))
       (c0 (car (stream-car input)) (stream-cdr input)))
-     ((not (null? (accept closure)))
-      (let ((item (car (accept closure))))
-	((list-ref (cons c0 continuations)
-		   (length (item-rhs item)))
-	 (item-lhs item) input)))
+     ((find-lookahead-item (accept closure) k input)
+      => (lambda (item)
+	   ((list-ref (cons c0 continuations)
+		      (length (item-rhs item)))
+	    (item-lhs item) input)))
      (else 'error))))
 
-(define (parse grammar input)
+(define (parse grammar k input)
   (cps-parse
-   grammar
+   grammar k (compute-first grammar k)
    (list (make-item (grammar-start-production grammar) 0 '()))
    '()
    input))
