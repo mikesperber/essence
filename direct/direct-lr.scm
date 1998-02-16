7a79915fda9c7245a7c83314a0193443d70b31fc
sperber <devnull@localhost>
1998-02-16 15:31:59 +0100

    Overhaul.

--- direct/direct-lr.scm
+++ direct/direct-lr.scm
@@ -1,73 +1,76 @@
 ;; Essential LR parsing (direct style)
 ;; ===================================
 
-(define (parse grammar k compute-closure state input)
+(define-memo _memo 1)
+
+(define (ds-parse grammar k compute-closure state input)
   (_memo
    (let ((closure (compute-closure state grammar)))
 
-     (if (final? state grammar)
-	 (if (equal? (make-eoi-terminal) (car input))
-	     'accept
-	     (_error "parse error"))
-	 (the-trick
-	  (car input) (next-terminals closure grammar)
-	  (lambda (symbol)
-	    (parse-bar grammar k compute-closure closure
-		       symbol (cdr input)))
-	  (lambda ()
-	    (select-lookahead-item
-	     (accept closure) k input
-	     (lambda (item)
-	       (let* ((rhs-length (length (item-rhs item)))
-		      (lhs (item-lhs item)))
-		 (if (zero? rhs-length)
-		     (parse-bar grammar k compute-closure closure lhs input)
-		     (parse-result lhs rhs-length input))))
-	     (lambda ()
-	       (_error "parse error")))))))))
+     (define (reduce)
+       (cond
+	((find-lookahead-item (accept closure) k input)
+	 => (lambda (item)
+	      (let ((rhs-length (length (item-rhs item)))
+		    (lhs (item-lhs item)))
+		(if (zero? rhs-length)
+		    (ds-parse-bar grammar k compute-closure
+				  closure lhs input)
+		    (parse-result lhs rhs-length input)))))
+	(else (_error "parse error"))))
 
-(define (parse-bar grammar k compute-closure closure symbol input)
-  (let* ((next-state (goto closure symbol))
-	 (result (parse grammar k compute-closure next-state input)))
-    (if (final? next-state grammar)
-	result
-	(let* ((lhs (result-lhs result))
-	       (dot (result-dot result))
-	       (inp (result-inp result))
-	       (nts (next-nonterminals closure grammar)))
-	  (cond ((null? nts) (parse-result lhs (- dot 1) inp))
-		((> dot 1) (parse-result lhs (- dot 1) inp))
-		(else
-		 (the-trick-cannot-fail
-		  lhs (next-nonterminals closure grammar)
-		  (lambda (symbol)
-		    (parse-bar grammar k compute-closure closure 
-			       symbol inp)))))))))
+     (cond
+      ((stream-empty? input) (reduce))
+      ((maybe-the-member (car (stream-car input))
+			 (next-terminals closure grammar))
+       => (lambda (symbol)
+	    (ds-parse-bar grammar k compute-closure closure
+			  symbol (stream-cdr input))))
+      (else (reduce))))))
 
-;; ~~~~~~~~~~~~
+(define (ds-parse-bar grammar k compute-closure closure symbol input)
+  (if (and (initial? closure grammar)
+	   (equal? (grammar-start grammar) symbol))
+      'accept
+      (let* ((result (ds-parse grammar k compute-closure
+			       (goto closure symbol) input))
+	     (lhs (result-lhs result))
+	     (dot (result-dot result))
+	     (input (result-input result)))
 
-(define (do-parse source-grammar k method input)
-  (let* ((grammar (source-grammar->grammar source-grammar k))
-	 (start-production (grammar-start-production grammar))
-	 (first-map (compute-first grammar k)))
+	(cond
+	 ((> dot 1)
+	  (parse-result lhs (- dot 1) input))
+	 ((and (initial? closure grammar)
+	       (equal? (grammar-start grammar) lhs))
+	  'accept)
+	 (else
+	  (ds-parse-bar grammar k compute-closure
+			closure
+			(the-member lhs (next-nonterminals closure grammar))
+			input))))))
 
-    (cond
-     ((equal? method 'lr)
-      (parse grammar
-	     k
-	     (lambda (state grammar)
-	       (compute-lr-closure state grammar k first-map))
-	     (list (make-item start-production
-			      0
-			      (cdr (production-rhs start-production))))
-	     input))
-     ((equal? method 'slr)
-      (let ((follow-map (compute-follow grammar k first-map)))
-	(parse grammar
-	       k
-	       (lambda (state grammar)
-		 (compute-slr-closure state grammar k follow-map))
-	       (add-slr-lookahead (list (make-item start-production 0 #f))
-				  follow-map)
-	       input))))))
+(define (parse-result lhs dot inp)
+  (vector lhs dot inp))
+(define (result-lhs result)
+  (vector-ref result 0))
+(define (result-dot result)
+  (vector-ref result 1))
+(define (result-input result)
+  (vector-ref result 2))
+
+(define (parse grammar k method input)
+  (let ((start-production (grammar-start-production grammar))
+	(first-map (compute-first grammar k)))
+
+    (ds-parse grammar
+	      k
+	      (if (equal? method 'lr)
+		  (lambda (state grammar)
+		    (compute-lr-closure state grammar k first-map))
+		  (let ((follow-map (compute-follow grammar k first-map)))
+		    (lambda (state grammar)
+		      (compute-slr-closure state grammar k follow-map))))
+	      (list (make-item start-production 0 '()))
+	      input)))
 
