8e2e8447cf848b38e5b239dc9100d25a9b98e43a
sperber <devnull@localhost>
1998-04-21 12:21:13 +0200

    Straight from Mørk.

---
+++ common/scc-union.scm
@@ -0,0 +1,46 @@
+; Digraph algorithm from Pennello/DeRemer
+; =======================================
+
+; This function is very generic, imperative, kludgy,
+; and it has too many arguments
+
+(define (complete-subsets! for-each-a a-equal? for-each-R
+			   associate-depth! depth-association
+			   f-association merge!)
+  (let ((stack '())
+	(depth 0))
+
+    (define (depth-min a b)
+      (cond ((not a) b)
+	    ((not b) a)
+	    (else (min a b))))
+
+    (define (descend! a)
+      (set! stack (cons a stack))
+      (set! depth (+ 1 depth))
+      (let ((depth depth))
+	(associate-depth! a depth)
+	(for-each-R
+	 (lambda (b)
+	   (if (eqv? 0 (depth-association b)) ; can't use zero? 'cause it may be #f
+	       (descend! b))
+	   (associate-depth!
+	    a
+	    (depth-min (depth-association a)
+		       (depth-association b)))
+	 
+	   (merge! (f-association a) (f-association b)))
+	 a)
+      
+	(if (= (depth-association a) depth)
+	    (let loop ()
+	      (associate-depth! (car stack) #f)
+	      (let ((top (car stack)))
+		(set! stack (cdr stack))
+		(if (not (a-equal? top a))
+		    (loop)))))))
+
+    (for-each-a (lambda (a)
+		  (if (eqv? 0 (depth-association a))
+		      (descend! a))))))
+	      
