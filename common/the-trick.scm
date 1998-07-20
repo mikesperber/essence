4c2b138b00199d2f3425e257c1ebab8338417e39
sperber <devnull@localhost>
1998-07-20 11:48:22 +0200

    Inserted memo2 point.

--- common/the-trick.scm
+++ common/the-trick.scm
@@ -1,15 +1,15 @@
 (define-without-memoization (maybe-the-member element set)
   (let loop ((set set))
-    (cond
-     ((null? set) #f)
-     ((equal? element (car set))
-      (car set))
-     (else
-      (loop (cdr set))))))
+    (_memo2
+     (cond
+      ((null? set) #f)
+      ((eqv? element (car set)) (car set))
+      (else (loop (cdr set)))))))
 
 (define-without-memoization (the-member element set)
   (let loop ((set set))
-    (cond
-     ((null? (cdr set)) (car set))
-     ((equal? element (car set)) (car set))
-     (else (loop (cdr set))))))
+    (_memo2
+     (cond
+      ((null? (cdr set)) (car set))
+      ((eqv? element (car set)) (car set))
+      (else (loop (cdr set)))))))
