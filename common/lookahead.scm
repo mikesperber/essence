93e17ca54da99297fa4ef3773749dcb5565c7514
sperber <devnull@localhost>
1998-07-20 11:45:33 +0200

    Restored the naive version. Leave the fancy stuff to lookahead-trie.scm.

--- common/lookahead.scm
+++ common/lookahead.scm
@@ -1,26 +1,22 @@
 (define-without-memoization
   (lookahead-matches? k lookahead input)
-  (let loop ((remaining k) (lookahead lookahead) (input input))
+  (let loop ((k k) (lookahead lookahead) (input input))
     (cond
-     ((zero? remaining) #t)
-     ((null? lookahead)
-      (if (= k remaining) ; we covered this previously
-	  #f
-	  (stream-empty? input)))
-     ((and (not (= k remaining)) ; we covered this previously
-	   (stream-empty? input))
-       #f)
+     ((zero? k) #t)
+     ((null? lookahead) (stream-empty? input))
+     ((stream-empty? input) #f)
      ((equal? (car lookahead) (car (stream-car input)))
-      (loop (- remaining 1) (cdr lookahead) (stream-cdr input)))
+      (loop (- k 1) (cdr lookahead) (stream-cdr input)))
      (else #f))))
 
 (define-without-memoization
   (find-lookahead-item item-set k input)
   (let loop ((item-set item-set))
-    (if (null? item-set)
-	#f
-	(let ((item (car item-set)))
-	  (if (lookahead-matches? k (item-lookahead item) input)
-	      item
-	      (loop (cdr item-set)))))))
+    (_memo2
+     (if (null? item-set)
+	 #f
+	 (let ((item (car item-set)))
+	   (if (lookahead-matches? k (item-lookahead item) input)
+	       item
+	       (loop (cdr item-set))))))))
 
