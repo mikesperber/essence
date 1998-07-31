bede16117a12a2c1f8d5884efb13e226832fb809
sperber <devnull@localhost>
1998-07-31 11:27:19 +0200

    Obsolete.

--- common/lr-runtime.scm
+++
@@ -1,9 +0,0 @@
-(define (move-error-status error-status)
-  (if (zero? error-status) error-status (- error-status 1)))
-
-(define (advance-input error-status input)
-  (cond
-   ((zero? error-status) input)
-   ((stream-empty? input)
-    (error "parse error: premature end of input"))
-   (else (stream-cdr input))))
