d855af3cf108579e333ded39fb7846a490c02c7d
sperber <devnull@localhost>
1998-07-31 11:29:11 +0200

    Now multi-stage-enabled.

--- common/the-trick-binary.scm
+++ common/the-trick-binary.scm
@@ -1,42 +1,44 @@
 (define-without-memoization (maybe-the-member element set)
   (let loop ((sorted (sort-list set <)) (size (length set)))
-    (cond
-     ((null? sorted) #f)
-     ((>= size 3)
-      (let* ((middle (- (quotient (+ size 1) 2) 1))
-	     (static-element (list-ref sorted middle)))
-	(cond
-	 ((eqv? static-element element)
-	  static-element)
-	 ((< element static-element)
-	  (loop (take middle sorted) middle))
-	 (else
-	  (loop (list-tail sorted (+ middle 1))
-		(- size (+ middle 1)))))))
-     ((eqv? element (car sorted))
-      (car sorted))
-     (else
-      (loop (cdr sorted) (- size 1))))))
+    (_memo2
+     (cond
+      ((null? sorted) #f)
+      ((>= size 3)
+       (let* ((middle (- (quotient (+ size 1) 2) 1))
+	      (static-element (list-ref sorted middle)))
+	 (cond
+	  ((eqv? static-element element)
+	   static-element)
+	  ((< element static-element)
+	   (loop (take middle sorted) middle))
+	  (else
+	   (loop (list-tail sorted (+ middle 1))
+		 (- size (+ middle 1)))))))
+      ((eqv? element (car sorted))
+       (car sorted))
+      (else
+       (loop (cdr sorted) (- size 1)))))))
 
 (define-without-memoization (the-member element set)
   (let loop ((sorted (sort-list set <)) (size (length set)))
-    (cond
-     ((= size 1) (car sorted))
-     ((>= size 3)
-      (let* ((middle (- (quotient (+ size 1) 2) 1))
-	     (static-element (list-ref sorted middle)))
-	(cond
-	 ((eqv? static-element element)
-	  static-element)
-	 ((< element static-element)
-	  (loop (take middle sorted) middle))
-	 (else
-	  (loop (list-tail sorted (+ middle 1))
-		(- size (+ middle 1)))))))
-     ((eqv? element (car sorted))
-      (car sorted))
-     (else
-      (loop (cdr sorted) (- size 1))))))
+    (_memo2
+     (cond
+      ((= size 1) (car sorted))
+      ((>= size 3)
+       (let* ((middle (- (quotient (+ size 1) 2) 1))
+	      (static-element (list-ref sorted middle)))
+	 (cond
+	  ((eqv? static-element element)
+	   static-element)
+	  ((< element static-element)
+	   (loop (take middle sorted) middle))
+	  (else
+	   (loop (list-tail sorted (+ middle 1))
+		 (- size (+ middle 1)))))))
+      ((eqv? element (car sorted))
+       (car sorted))
+      (else
+       (loop (cdr sorted) (- size 1)))))))
 
 (define (take n l)
   (let loop ((n n) (l l) (result '()))
