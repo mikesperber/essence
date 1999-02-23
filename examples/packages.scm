8dd97ca5fc3feef755d8e0b40f7625999106b718
sperber <devnull@localhost>
1999-02-23 15:55:16 +0100

    Long obsolete.

--- examples/packages.scm
+++
@@ -1,6 +0,0 @@
-(define-interface toy-grammars-interface
-  (export g10 (g10-symbol :syntax) i10-1 i10-2 i10-3))
-
-(define-structure toy-grammars toy-grammars-interface
-  (open scheme enumerated grammar)
-  (files toy-grammars))
