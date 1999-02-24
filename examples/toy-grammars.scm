f3211bfd4d5b784fd8b19bdc81e8a95763c29636
sperber <devnull@localhost>
1999-02-24 10:05:42 +0100

    Separated toy-inputs out of toy-grammars.

--- examples/toy-grammars.scm
+++ examples/toy-grammars.scm
@@ -1,4 +1,4 @@
-; Balanced parenthesis
+; Balanced parentheses
 
 (define-grammar g00 g00-symbol
   (S)
@@ -15,28 +15,6 @@
    ((T l S r) $1)
    ((T l r) $1)))
 
-(define i08-1 (list (cons (enum g08-symbol l) #f)
-		    (cons (enum g08-symbol r) #f)
-		    (cons (enum g08-symbol l) #f)
-		    (cons (enum g08-symbol l) #f)
-		    (cons (enum g08-symbol r) #f)
-		    (cons (enum g08-symbol r) #f)))
-
-(define i08-2 (list (cons (enum g08-symbol l) #f)
-		    (cons (enum g08-symbol l) #f)
-		    (cons (enum g08-symbol l) #f)
-		    (cons (enum g08-symbol l) #f)
-		    (cons (enum g08-symbol r) #f)
-		    (cons (enum g08-symbol r) #f)))
-
-(define i08-3 (list (cons (enum g08-symbol l) #f)
-		    (cons (enum g08-symbol r) #f)
-		    (cons (enum g08-symbol l) #f)
-		    (cons (enum g08-symbol l) #f)
-		    (cons (enum g08-symbol r) #f)
-		    (cons (enum g08-symbol r) #f)
-		    (cons (enum g08-symbol l) #f)))
-
 ; Constant arithmetic expressions
 
 (define-grammar g10 g10-symbol
@@ -50,34 +28,7 @@
    ((T P * T) (* $1 $3))
    ((T P / T) (/ $1 $3))
    ((P n) $1)
-   ((P l E r) $2))
-  (if (eq? terminal 'n)
-      42
-      #f))
-
-(define i10-1 (list (cons (enum g10-symbol l) #f)
-		    (cons (enum g10-symbol n) 4)
-		    (cons (enum g10-symbol +) #f)
-		    (cons (enum g10-symbol n) 17)
-		    (cons (enum g10-symbol r) #f)
-		    (cons (enum g10-symbol *) #f)
-		    (cons (enum g10-symbol n) 7)))
-
-(define i10-2 (list (cons (enum g10-symbol l) #f)
-		    (cons (enum g10-symbol n) 4)
-		    (cons (enum g10-symbol +) #f)
-		    (cons (enum g10-symbol n) 15)
-		    (cons (enum g10-symbol n) 17)
-		    (cons (enum g10-symbol r) #f)
-		    (cons (enum g10-symbol *) #f)
-		    (cons (enum g10-symbol n) 7)))
-
-(define i10-3 (list (cons (enum g10-symbol l) #f)
-		    (cons (enum g10-symbol n) 4)
-		    (cons (enum g10-symbol +) #f)
-		    (cons (enum g10-symbol n) 17)
-		    (cons (enum g10-symbol *) #f)
-		    (cons (enum g10-symbol n) 7)))
+   ((P l E r) $2)))
 
 (define-grammar g10-error g10-error-symbol
   (E T P)
@@ -92,31 +43,6 @@
    ((T P / T) (/ $1 $3))
    ((P n) $1)
    ((P l E r) $2)
-   ((P l $error r) 0))
-  (if (eq? terminal 'n)
-      42
-      #f))
+   ((P l $error r) 0)))
 
-(define i10e-1 (list (cons (enum g10-error-symbol l) #f)
-		     (cons (enum g10-error-symbol n) 4)
-		     (cons (enum g10-error-symbol +) #f)
-		     (cons (enum g10-error-symbol n) 17)
-		     (cons (enum g10-error-symbol r) #f)
-		     (cons (enum g10-error-symbol *) #f)
-		     (cons (enum g10-error-symbol n) 7)))
 
-(define i10e-2 (list (cons (enum g10-error-symbol l) #f)
-		     (cons (enum g10-error-symbol n) 4)
-		     (cons (enum g10-error-symbol +) #f)
-		     (cons (enum g10-error-symbol n) 15)
-		     (cons (enum g10-error-symbol n) 17)
-		     (cons (enum g10-error-symbol r) #f)
-		     (cons (enum g10-error-symbol *) #f)
-		     (cons (enum g10-error-symbol n) 7)))
-
-(define i10e-3 (list (cons (enum g10-error-symbol l) #f)
-		     (cons (enum g10-error-symbol n) 4)
-		     (cons (enum g10-error-symbol +) #f)
-		     (cons (enum g10-error-symbol n) 17)
-		     (cons (enum g10-error-symbol *) #f)
-		     (cons (enum g10-error-symbol n) 7)))
